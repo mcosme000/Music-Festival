b30d338cc81c3f6b0b3ddda9c55b588efdd4d3cf
Maria Cosmen <mcosme00@gmail.com>
2021-05-01 12:42:00 +0900

    ticket section & smooth scroll

--- build/js/bundle.js
+++ build/js/bundle.js
@@ -1,5 +1,41 @@
 "use strict";
 
+//SMOOTH SCROLL:
+
+document.addEventListener("DOMContentLoaded", () => {
+  scrollNav();
+});
+
+function scrollNav() {
+  const links = document.querySelectorAll(".header-nav a");
+  links.forEach((link) => {
+    link.addEventListener("click", (e) => {
+      e.preventDefault();
+
+      console.log(e.target.attributes.href.value);
+
+      //this is the section we are clickin in to.
+      const section = document.querySelector(e.target.attributes.href.value);
+
+      section.scrollIntoView({
+        behavior: "smooth",
+      });
+    });
+  });
+}
+
+// CREATING THE SMOOTH SCROLL:
+/**
+1. when we have a group of elements, we need to iterate through them to
+    add event listeners to each of the links.
+
+2. console.log(e.target.attributes.href);
+    nos muestra el href="#tickets"
+
+3. console.log(e.target.attributes.href.value);
+    nos indica a dónde va el link
+ */
+
 document.addEventListener("DOMContentLoaded", () => {
   createGallery();
 });
